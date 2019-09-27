e88e498175ee82681e158251d0bd2fe22b7232dd
Raul <46034193+KilliN-SM@users.noreply.github.com>
2019-09-27 06:07:15 -0300

    Added "Clear cache" button

--- js/classes/WUPlusNSettingsTab.js
+++ js/classes/WUPlusNSettingsTab.js
@@ -88,6 +88,7 @@ class WUPlusNSettingsTab extends HTMLElement
         const switchDivineTier = newSwitchContainer('Divine Tier', $.getLS('divine_tier'), switchDivineTierEvent);
         const buttonCustomItems = new WUButton('Custom Items', './img/general/customitems.png', buttonCustomItemsEvent);
         const buttonMechsList = new WUButton('Your Mechs', './img/general/mech.svg', buttonMechsListEvent);
+        const buttonClearCache = new WUButton('Clear cache and refresh', './img/general/x.svg', () => window.location.reload(true));
         const buttonCloseTab = new WUButton('Close', './img/general/x.svg', () => this.hide());
 
         buttonCloseTab.classList.add('close');
@@ -100,6 +101,7 @@ class WUPlusNSettingsTab extends HTMLElement
         contentWrapper.appendChild(switchDivineTier);
         contentWrapper.appendChild(buttonCustomItems);
         contentWrapper.appendChild(buttonMechsList);
+        contentWrapper.appendChild(buttonClearCache);
         contentWrapper.appendChild(buttonCloseTab);
 
         window.workshop.appendChild(tabCustomItems);
@@ -110,55 +112,6 @@ class WUPlusNSettingsTab extends HTMLElement
         });
 
         this.hide();
-
-        /*
-        this.className = 'tab';
-
-        const customItemsTab = new WUCustomItemsTab();
-        const cw = $.dom('content-wrapper', { className:'box border' });
-        const switchsContainer = $.dom('switchs-container');
-        const arenaBuffsSwitch = new WUSwitchContainer('Arena Buffs', $.getLS('arena_buffs'), function(e)
-        {
-            $.setLS('arena_buffs', Boolean(e.target.checked));
-            window.workshop.updateMechSummary();
-        });
-        const divineTierSwitch = new WUSwitchContainer('Divine Tier (off)', $.getLS('divine_tier'), e =>
-        {
-            $.setLS('divine_tier', Boolean(e.target.checked));
-            window.workshop.updateMechSummary();
-        });
-        const arenaBuffsOffsetSwitch = new WUSwitchContainer('Arena Buffs Offset', $.getLS('arena_buffs_offset'), e =>
-        {
-            $.setLS('arena_buffs_offset', Boolean(e.target.checked));
-            window.workshop.updateMechSummary();
-        });
-        const customItemsButton = new WUButton({ text:'Custom Items' }, './img/general/customitems.png', () =>
-        {
-            this.hide();
-            customItemsTab.show();
-        });
-        const mechsListButton = new WUButton({ text:'Your Mechs' }, './img/general/mech.svg', () =>
-        {
-            console.log('mechsListTab.show();');
-        });
-
-        switchsContainer.style.gridArea  = 'a';
-        customItemsButton.style.gridArea = 'b';
-        mechsListButton.style.gridArea   = 'c';
-
-        this.onclick = e => { e.target === this && this.hide() };
-
-        switchsContainer.appendChild(arenaBuffsSwitch);
-        switchsContainer.appendChild(divineTierSwitch);
-        switchsContainer.appendChild(arenaBuffsOffsetSwitch);
-        cw.appendChild(switchsContainer);
-        cw.appendChild(customItemsButton);
-        cw.appendChild(mechsListButton);
-        this.appendChild(cw);
-        window.workshop.appendChild(customItemsTab);
-
-        this.hide();
-        */
     }
 
     show ()
