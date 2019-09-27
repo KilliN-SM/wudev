d8c71194d463d39220f07615097c4787ef9ea9ad
Raul <46034193+KilliN-SM@users.noreply.github.com>
2019-09-27 06:43:19 -0300

    Added fullscreen for mobile

--- js/classes/WUStatBlock.js
+++ js/classes/WUStatBlock.js
@@ -4,7 +4,9 @@ class WUStatBlock extends HTMLElement
     {
         super();
 
-        this.hoverData = { text:statData.context };
+        const hoverData = { text:statData.context };
+
+        this.hoverData = hoverData;
 
         if (Array.isArray(value))
         {
@@ -14,8 +16,8 @@ class WUStatBlock extends HTMLElement
             }
         }
 
-        const icon = $.css($.dom('img', { hoverData:{ text:statData.context }, src:statData.src }), { gridArea:'icon' });
-        const text = $.css($.dom('div', { hoverData:{ text:statData.context }, innerText:String(value).replace(',', '-') }), { gridArea:'text' });
+        const icon = $.css($.dom('img', { hoverData, src:statData.src }), { gridArea:'icon' });
+        const text = $.css($.dom('div', { hoverData, innerText:String(value).replace(',', '-') }), { gridArea:'text' });
         
         this.appendChild(icon);
         this.appendChild(text);
